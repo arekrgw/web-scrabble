7a74cb0e3d300647f3809a8af5a9608b2818a827
Jason <adaglo524@gmail.com>
2021-05-28 10:43:45 +0200

    css grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,7 @@
 const App = () => {
   return (
     <div>
-      <header>YO</header>
-      <p>Happy Coding</p>
-      <p>Happy Coding</p>
-      <p>Happy Coding</p>
-      <p>Happy Coding</p>
-      <h1>New HEADER</h1>
-      <button>Click 3</button>
-      <button>Click 3</button>
+    <div class="grid-container"></div>
     </div>
   );
 };

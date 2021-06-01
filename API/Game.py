95c3c3365caac9ce8f4530b77f3cc23e13834a34
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-06-01 16:32:31 +0200

    game status flag and player count

--- API/Game.py
+++ API/Game.py
@@ -2,6 +2,8 @@ class Game():
     def __init__(self):
         file = open("slownik.txt", "r", encoding='utf8')
         self.dictionary = file.readlines()
+        self.game_status=False
+        self.connected_player=0
 
     def checkWord(self, toCheck):
         for i in self.dictionary:
@@ -9,3 +11,13 @@ class Game():
             if i == toCheck:
                 return True
         return False
+
+    def getConnected_players(self):
+        return self.connected_player
+
+    def Connected_player(self):
+        self.connected_player=self.connected_player+1
+
+    def setGameStart(self):
+        self.game_status=True;
+

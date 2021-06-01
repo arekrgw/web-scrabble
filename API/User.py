2ca2a805e32a94c297048a7c27c7b35881bda426
Adrian Pietrzak <78472697+AdrianPietrz@users.noreply.github.com>
2021-06-01 16:33:41 +0200

    added id generation

--- API/User.py
+++ API/User.py
@@ -1,13 +1,22 @@
+import uuid
+
 class User():
-    def __init__(self,connection_id, name):
+    def __init__(self,connection_id, name,id):
         self.user_id=connection_id
+        if id==None:
+            self.id=uuid.uuid4().hex
+        else:
+            self.id=id
         self.name=name
         self.score=0
         self.letters=[]
         self.player_ready=False
 
-    def getID(self):
+    def getUserID(self):
         return self.user_id
+
+    def getID(self):
+        return self.id
 
     def getScore(self):
         return self.score

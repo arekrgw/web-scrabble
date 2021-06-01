f6387f5b29d7d0822db130f41f2b7fdeb1e5fe1b
Jason <adaglo524@gmail.com>
2021-06-01 20:56:59 +0200

    basic board shape

--- client/src/pages/GamePage/index.js
+++ client/src/pages/GamePage/index.js
@@ -1,5 +1,26 @@
+import { SimpleGrid ,Grid, Box, GridItem, Input, FormControl, FormLabel, Button, Container } from '@chakra-ui/react';
+import { useState } from 'react';
+import BoardTile from '../../components/BoardTile';
+import {fieldsData} from './filedsData'
+
 const GamePage = () => {
-  return <h1>Game Page</h1>;
+  
+  return (
+    <Container centerContent>
+    <Grid marginTop="200px"
+    h="750px"
+    w="750px"
+    >
+      <Grid templateColumns="repeat(15, 0fr)" templateRows="repeat(15,0fr)" bg="grey" gap={0}>
+      {fieldsData.map(field => (
+        console.log(field),
+        <BoardTile key={field} value={field}/>
+      ))}
+      </Grid>
+    </Grid>
+    </Container>
+
+  );
 };
 
 export default GamePage;
